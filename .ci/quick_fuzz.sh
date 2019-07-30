9c2c9d8c17ee80ce81c822a4c9b86a43fe1f5fc8
Philip Top <top1@llnl.gov>
2019-07-30 13:30:53 -0700

    add a fuzz script

---
+++ .ci/quick_fuzz.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo -en "travis_fold:start:script.build\\r"
+echo "Building..."
+STD=$1
+shift
+set -evx
+
+
+mkdir -p build
+cd build
+cmake .. -DUNITS_CXX_STD=14 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DBUILD_UNITS_FUZZ_TARGETS=ON $@
+cmake --build . -- -j2
+
+set +evx
+echo -en "travis_fold:end:script.build\\r"
+echo -en "travis_fold:start:script.test\\r"
+echo "Testing..."
+set -evx
+
+ctest --output-on-failure
+
+set +evx
+echo -en "travis_fold:end:script.test\\r"
