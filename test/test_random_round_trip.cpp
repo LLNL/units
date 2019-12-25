1fe36c7c1d21afacad5f3f88de44febf24a2eb55
Philip Top <phlptp@gmail.com>
2019-12-25 06:08:12 -0800

    fix a failing test on some systems for double comparison and a few warning fixes

--- test/test_random_round_trip.cpp
+++ test/test_random_round_trip.cpp
@@ -22,7 +22,7 @@ TEST(randomRoundTrip, basic)
     for (auto ii = 0; ii < 6000; ++ii) {
         auto start = distribution(generator);
         detail::unit_data unitdata(nullptr);
-        memcpy(&unitdata, &start, 4);
+        memcpy(static_cast<void *>(&unitdata), &start, 4);
         auto startunit = unit(unitdata);
         auto str = to_string(startunit);
         auto resunit = unit_cast(unit_from_string(str));
@@ -37,7 +37,7 @@ TEST_P(rtrip, testConversions)
 {
     unsigned int start = GetParam();
     detail::unit_data unitdata(nullptr);
-    memcpy(&unitdata, &start, 4);
+    memcpy(static_cast<void *>(&unitdata), &start, 4);
     auto startunit = unit(unitdata);
     auto str = to_string(startunit);
     auto resunit = unit_cast(unit_from_string(str));
