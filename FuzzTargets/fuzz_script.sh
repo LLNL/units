fc9e51a4ef94993eb873e42deb7233ec23c8a957
Philip Top <top1@llnl.gov>
2020-01-15 17:28:34 -0800

    add a fuzzing test

--- FuzzTargets/fuzz_script.sh
+++ FuzzTargets/fuzz_script.sh
@@ -2,4 +2,11 @@
 
 mkdir -p /fuzz/corpus
 
-./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=512 -max_total_time=1200 -timeout=10 -artifact_prefix=/fuzz/
+./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=712 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
+./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=712 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
+
+mkdir -p new_corpus
+./fuzz_from_string new_corpus /fuzz/corpus -merge=1
+
+rm /fuzz/corpus/*
+mv new_corpus/* /fuzz/corpus/
