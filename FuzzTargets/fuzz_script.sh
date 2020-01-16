6dc9a24b8799f6454602c2e07b288f3375989f75
Philip Top <phlptp@gmail.com>
2020-01-16 08:01:39 -0800

    update docker image and script

--- FuzzTargets/fuzz_script.sh
+++ FuzzTargets/fuzz_script.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /fuzz/corpus
 
-./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=712 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
-./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=712 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
+./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=600 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
+./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=600 -max_total_time=600 -timeout=10 -artifact_prefix=/fuzz/
 
 mkdir -p new_corpus
 ./fuzz_from_string new_corpus /fuzz/corpus -merge=1
