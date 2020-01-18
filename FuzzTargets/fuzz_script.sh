66c3165ad6b078a80e68a64cca011c88db1cf010
Philip Top <phlptp@gmail.com>
2020-01-17 20:20:10 -0800

    change the fuzzing to a single run of 20 minutes

--- FuzzTargets/fuzz_script.sh
+++ FuzzTargets/fuzz_script.sh
@@ -3,37 +3,13 @@
 mkdir -p /fuzz/corpus
 
 # run the first fuzzing sequence
-./fuzz_from_string /fuzz/corpus -max_len=384 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=685 -max_total_time=300 -timeout=7 -artifact_prefix=/fuzz/
-
-#if we haven't failed run another
-if [ $? -eq 0 ]
-then
-   ./fuzz_from_string /fuzz/corpus -max_len=384 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=685 -max_total_time=300 -timeout=7 -artifact_prefix=/fuzz/
-else
-  exit 1
-fi 
-
-#if we haven't failed run another 
-if [ $? -eq 0 ]
-then
-   ./fuzz_from_string /fuzz/corpus -max_len=384 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=685 -max_total_time=300 -timeout=7 -artifact_prefix=/fuzz/
-else
-  exit 1
-fi 
-
-#if we haven't failed run another 
-if [ $? -eq 0 ]
-then
-   ./fuzz_from_string /fuzz/corpus -max_len=384 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=685 -max_total_time=300 -timeout=7 -artifact_prefix=/fuzz/
-else
-  exit 1
-fi 
+./fuzz_from_string /fuzz/corpus -max_len=512 -dict=/root/develop/fuzz_targets/fuzz_dictionary.txt -rss_limit_mb=600 -max_total_time=1200 -timeout=5 -artifact_prefix=/fuzz/
 
 #if we haven't failed merge the corpus so it is smaller
 if [ $? -eq 0 ]
 then
   mkdir -p new_corpus
-  ./fuzz_from_string new_corpus /fuzz/corpus -merge=1 -max_len=384 -max_total_time=120
+  ./fuzz_from_string new_corpus /fuzz/corpus -merge=1 -max_len=512 -max_total_time=120
   if [ $? -eq 0 ]
   then
     rm /fuzz/corpus/*
