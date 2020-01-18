4e56de05ed7d0869f30b20d627f3b85ee749120a
Philip Top <top1@llnl.gov>
2020-01-18 08:33:08 -0800

    Fuzz docker image (#16)
    * add a docker image for fuzzing
    
    * add fuzz_targets.sh
    
    * make script executable
    
    * fix warning on multiplier_f operations
    
    * add a fuzzing test
    
    * update fuzz targets
    
    * update fuzzing target and formatting
    
    * update docker image and script
    
    * add a docker image for fuzzing
    
    * update dockerfile with appropriate branch
    
    * fix warning on multiplier_f operations
    
    * update fuzzing target and formatting
    
    * update docker image and script
    
    * update docker file to just use the prebuild clang-bulider8
    
    * update the fuzz script and deal with empty strings passed into the fuzzing function
    
    * update docker file to turn off leak checking
    
    * update docker and script
    
    * remove undefined behavior sanitizer
    
    * try without address sanitizer
    
    * change the fuzzing to a single run of 24 minutes
    
    * add fuzzing test and fix
    
    * update the fuzzing script to save log files
    
    * fix the formatting and tweak the fuzzing script

--- FuzzTargets/fuzz_target_from_string.cpp
+++ FuzzTargets/fuzz_target_from_string.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2019,
+Copyright (c) 2019-2020,
 Lawrence Livermore National Security, LLC;
 See the top-level NOTICE for additional details. All rights reserved.
 SPDX-License-Identifier: BSD-3-Clause
@@ -13,6 +13,9 @@ static bool cflag = units::disableCustomCommodities();
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
+    if (Size == 0) {
+        return 0;
+    }
     std::string test1(reinterpret_cast<const char*>(Data), Size);
     auto unit1 = units::unit_from_string(test1);
     if (!is_error(unit1)) {
@@ -21,11 +24,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
         if (units::is_error(u2)) {
             throw(6u);
         }
-        if (units::unit_cast(u2) != units::unit_cast(unit1)) {
+        bool match = false;
+        if (units::unit_cast(u2) == units::unit_cast(unit1)) {
+            match = true;
+        } else if (units::isnormal(root(u2, 2))) {
+            if (root(units::unit_cast(u2), 2) == root(units::unit_cast(unit1), 2)) {
+                match = true;
+            }
+        }
+        if (!match) {
+            if (units::isnormal(root(u2, 3))) {
+                if (root(units::unit_cast(u2), 3) == root(units::unit_cast(unit1), 3)) {
+                    match = true;
+                }
+            }
+        }
+        if (!match) {
             if (std::isnormal(u2.multiplier()) && std::isnormal(unit1.multiplier())) {
                 throw(5.0);
             } else if (u2.base_units() != unit1.base_units()) {
                 throw(7);
+            } else {
+                throw("nan");
             }
         }
     }
--- FuzzTargets/fuzz_target_from_string_flags.cpp
+++ FuzzTargets/fuzz_target_from_string_flags.cpp
@@ -18,7 +18,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
         return 0;
     }
     std::string test1(reinterpret_cast<const char*>(Data + 4), Size - 4);
-    uint32_t flags;
+    std::uint32_t flags;
     std::memcpy(&flags, Data, 4);
     auto unit1 = units::unit_from_string(test1, flags);
     if (!units::is_error(unit1)) {
@@ -27,11 +27,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
         if (units::is_error(u2)) {
             throw(6u);
         }
-        if (units::unit_cast(u2) != units::unit_cast(unit1)) {
+        bool match = false;
+        if (units::unit_cast(u2) == units::unit_cast(unit1)) {
+            match = true;
+        } else if (isnormal(root(u2, 2))) {
+            if (root(units::unit_cast(u2), 2) == root(units::unit_cast(unit1), 2)) {
+                match = true;
+            }
+        }
+        if (!match) {
+            if (isnormal(root(u2, 3))) {
+                if (root(units::unit_cast(u2), 3) == root(units::unit_cast(unit1), 3)) {
+                    match = true;
+                }
+            }
+        }
+        if (!match) {
             if (std::isnormal(u2.multiplier()) && std::isnormal(unit1.multiplier())) {
                 throw(5.0);
             } else if (u2.base_units() != unit1.base_units()) {
                 throw(7);
+            } else {
+                throw("nan");
             }
         }
     }
