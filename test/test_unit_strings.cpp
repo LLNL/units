207d61f5c8fbb8939a36223149fa927cb33e0924
Philip Top <phlptp@gmail.com>
2020-01-21 20:59:25 -0800

    fix the broken test

--- test/test_unit_strings.cpp
+++ test/test_unit_strings.cpp
@@ -178,7 +178,7 @@ TEST(unitStrings, crazyunits)
     unit tc{detail::unit_data(1, 1, -3, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0)};
     str = to_string(tc);
     EXPECT_EQ(str, "W*m^-1*K^-1");
-    EXPECT_EQ(to_string(unit(0.1, pu)), "pu/10");
+    EXPECT_EQ(to_string(precise_unit(10, precise::pu)), "10*pu");
 }
 
 TEST(unitStrings, customUnits)
