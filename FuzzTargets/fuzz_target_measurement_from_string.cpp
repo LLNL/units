dee05e1d14cd9c9c75c74e8784de2fc785d3d5a4
Philip Top <phlptp@gmail.com>
2020-03-08 20:18:25 -0700

    add a fuzz target for measurement, and remove the fuzz target for string with flags since that didn't seem to do much.

---
+++ FuzzTargets/fuzz_target_measurement_from_string.cpp
@@ -0,0 +1,50 @@
+/*
+Copyright (c) 2019-2020,
+Lawrence Livermore National Security, LLC;
+See the top-level NOTICE for additional details. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include "units/units.hpp"
+#include <cstring>
+#include <exception>
+#include <string>
+
+static bool cflag = units::disableCustomCommodities();
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
+{
+	if (Size == 0) {
+		return 0;
+	}
+	std::string test1(reinterpret_cast<const char*>(Data), Size);
+
+    auto meas1 = units::measurement_from_string(test1);
+    if (!units::is_valid(meas1)) {
+        auto str = units::to_string(meas1);
+        auto meas2 = units::measurement_from_string(str);
+        if (units::is_valid(meas2)) {
+            throw(6u);
+        }
+        bool match = false;
+        if (units::measurement_cast(meas2) == units::measurement_cast(meas1)) {
+            match = true;
+        } else if (isnormal(root(meas2, 2))) {
+            if (root(units::measurement_cast(meas2), 2) == root(units::measurement_cast(meas1), 2)) {
+                match = true;
+            }
+        }
+        if (!match) {
+            if (isnormal(root(meas2, 3))) {
+                if (root(units::measurement_cast(meas2), 3) == root(units::measurement_cast(meas1), 3)) {
+                    match = true;
+                }
+            }
+        }
+        if (!match) {
+           throw("nan");
+        }
+    }
+    // its::clearCustomCommodities();
+    return 0; // Non-zero return values are reserved for future use.
+}
