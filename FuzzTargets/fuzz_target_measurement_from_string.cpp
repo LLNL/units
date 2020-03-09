e502d5a6777920d111122c06e95e5af5c8d5eaf0
Philip Top <top1@llnl.gov>
2020-03-09 09:57:49 -0700

    update circle ci to create artifacts for the measurement test

--- FuzzTargets/fuzz_target_measurement_from_string.cpp
+++ FuzzTargets/fuzz_target_measurement_from_string.cpp
@@ -14,10 +14,10 @@ static bool cflag = units::disableCustomCommodities();
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
-	if (Size == 0) {
-		return 0;
-	}
-	std::string test1(reinterpret_cast<const char*>(Data), Size);
+    if (Size == 0) {
+        return 0;
+    }
+    std::string test1(reinterpret_cast<const char*>(Data), Size);
 
     auto meas1 = units::measurement_from_string(test1);
     if (!units::is_valid(meas1)) {
@@ -27,22 +27,24 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
             throw(6u);
         }
         bool match = false;
-        if (units::measurement_cast(meas2) == units::measurement_cast(meas1)) {
+        auto mc1 = units::measurement_cast(meas1);
+        auto mc2 = units::measurement_cast(meas2);
+        if (mc1 == mc2) {
             match = true;
-        } else if (isnormal(root(meas2, 2))) {
-            if (root(units::measurement_cast(meas2), 2) == root(units::measurement_cast(meas1), 2)) {
+        } else if (isnormal(root(mc2, 2))) {
+            if (root(mc2, 2) == root(mc1, 2)) {
                 match = true;
             }
         }
         if (!match) {
-            if (isnormal(root(meas2, 3))) {
-                if (root(units::measurement_cast(meas2), 3) == root(units::measurement_cast(meas1), 3)) {
+            if (isnormal(root(mc2, 3))) {
+                if (root(mc2, 3) == root(mc1, 3)) {
                     match = true;
                 }
             }
         }
         if (!match) {
-           throw("nan");
+            throw(std::invalid_argument("measurement and conversion don't match");
         }
     }
     // its::clearCustomCommodities();
