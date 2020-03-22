23c485fa805d73c6734928fda272c62e7f633fbd
Philip Top <phlptp@gmail.com>
2020-03-22 15:02:23 -0700

    add more output for the test

--- test/test_r20.cpp
+++ test/test_r20.cpp
@@ -14,35 +14,44 @@ using unitD = std::tuple<const char*, const char*, units::precise_unit>;
 
 TEST(r20, order)
 {
-	std::size_t unit_count{ 0 };
-	const void *r20 = units::detail::testing::r20rawData(unit_count);
-	auto *r20data = reinterpret_cast<const unitD *>(r20);
-	for (size_t ii = 1; ii < unit_count; ++ii)
-	{
-		EXPECT_LT(std::string(std::get<0>(r20data[ii - 1])), std::string(std::get<0>(r20data[ii])));
-	}
-
+    std::size_t unit_count{0};
+    const void* r20 = units::detail::testing::r20rawData(unit_count);
+    auto* r20data = reinterpret_cast<const unitD*>(r20);
+    for (size_t ii = 1; ii < unit_count; ++ii) {
+        EXPECT_LT(
+            std::string(std::get<0>(r20data[ii - 1])),
+            std::string(std::get<0>(r20data[ii])));
+    }
 }
 
 TEST(r20, conversions)
 {
-	std::size_t unit_count{ 0 };
-	const void *r20 = units::detail::testing::r20rawData(unit_count);
-	auto *r20data = reinterpret_cast<const unitD *>(r20);
-	int missed{ 0 };
-	for (size_t ii = 1; ii < unit_count; ++ii)
-	{
-		std::string ustr = std::string(std::get<1>(r20data[ii]));
-		auto unit = units::unit_from_string(ustr);
-		if (is_valid(unit))
-		{
-			EXPECT_EQ(unit, std::get<2>(r20data[ii])) <<' '<< ii <<' '<< ustr << " not converted properly";
-			if (unit != std::get<2>(r20data[ii]))
+    std::size_t unit_count{0};
+    const void* r20 = units::detail::testing::r20rawData(unit_count);
+    auto* r20data = reinterpret_cast<const unitD*>(r20);
+    int missed{0};
+	int correct{ 0 };
+	int skipped{ 0 };
+    for (size_t ii = 1; ii < unit_count; ++ii) {
+        std::string ustr = std::string(std::get<1>(r20data[ii]));
+        auto unit = units::unit_from_string(ustr);
+        if (is_valid(unit)) {
+            EXPECT_EQ(unit, std::get<2>(r20data[ii]))
+                << ' ' << ii << ' ' << ustr << " not converted properly";
+            if (unit != std::get<2>(r20data[ii])) {
+                ++missed;
+            }
+			else
 			{
-				++missed;
+				++correct;
 			}
+        }
+		else
+		{
+			++skipped;
 		}
-	}
-	std::cout << missed << " r20 units not translated properly\n";
-
+    }
+    std::cout << missed << " r20 units not translated properly\n";
+	std::cout << skipped << " r20 units skipped\n";
+	std::cout << correct << " r20 units correctly translated\n";
 }
