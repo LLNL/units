53e917f547013ed997e0b542587e954052d9cadb
Philip Top <phlptp@gmail.com>
2020-03-22 06:32:41 -0700

    update formatting

--- units/units_decl.hpp
+++ units/units_decl.hpp
@@ -275,8 +275,8 @@ namespace detail {
         }
 
       private:
-		  /* check if the base_unit has a valid root
-		  @details, checks that all the flags */
+        /* check if the base_unit has a valid root
+        @details, checks that all the flags */
         constexpr bool hasValidRoot(int power) const
         {
             return meter_ % power == 0 && second_ % power == 0 &&
@@ -716,12 +716,12 @@ class precise_unit {
         return base_units_ == other.base_units_ &&
             commodity_ == other.commodity_ && multiplier_ == other.multiplier_;
     }
-	// Test for exact numerical equivalence
-	constexpr bool is_exactly_the_same(unit other) const
-	{
-		return base_units_ == other.base_units() &&
-			commodity_ == 0 && multiplier_ == other.multiplier();
-	}
+    // Test for exact numerical equivalence
+    constexpr bool is_exactly_the_same(unit other) const
+    {
+        return base_units_ == other.base_units() && commodity_ == 0 &&
+            multiplier_ == other.multiplier();
+    }
     /// Check if the units have the same base unit (i.e. they measure the same
     /// thing)
     constexpr bool has_same_base(precise_unit other) const
