a6d0867632c6aae18c51156ef7f081aaa8dec792
Philip Top <top1@llnl.gov>
2020-05-04 13:48:38 -0700

    add cmake format pre-commit check (#65)
    * add cmake format pre-commit check
    
    * try different cmake-format repo
    cmake-lint didn't work as well and I don't want to try to configure it right now
    
    * update cmake-format to latest version flags and move third party cmake code to different location.
    
    * update the repo version for cmake-format
    
    * update changelog with this commit updates

--- config/compiler_flags.cmake
+++ config/compiler_flags.cmake
@@ -8,9 +8,11 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 option(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS
-       "disable compiler warning for ${CMAKE_PROJECT_NAME} build" ON)
+       "disable compiler warning for ${CMAKE_PROJECT_NAME} build" ON
+)
 option(${PROJECT_NAME}_ENABLE_ERROR_ON_WARNINGS
-       "generate a compiler error for any warning encountered" OFF)
+       "generate a compiler error for any warning encountered" OFF
+)
 
 mark_as_advanced(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
 mark_as_advanced(${PROJECT_NAME}_ENABLE_ERROR_ON_WARNINGS)
@@ -32,19 +34,18 @@ target_compile_options(
 
 if(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
     target_compile_options(
-        compile_flags_target
-        INTERFACE $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -pedantic>
+        compile_flags_target INTERFACE $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall
+                                       -pedantic>
     )
     target_compile_options(
         compile_flags_target
-        INTERFACE
-            $<$<COMPILE_LANGUAGE:CXX>:$<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wextra
-            -Wshadow
-            -Wstrict-aliasing=1
-            -Wunreachable-code
-            -Woverloaded-virtual
-            -Wdouble-promotion
-            -Wundef>>
+        INTERFACE $<$<COMPILE_LANGUAGE:CXX>:$<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wextra
+                  -Wshadow
+                  -Wstrict-aliasing=1
+                  -Wunreachable-code
+                  -Woverloaded-virtual
+                  -Wdouble-promotion
+                  -Wundef>>
     )
 
     target_compile_options(
@@ -78,8 +79,8 @@ if(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
         if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 6.0)
             target_compile_options(
                 compile_flags_target
-                INTERFACE
-                    $<$<COMPILE_LANGUAGE:CXX>:-Wduplicated-cond -Wnull-dereference>
+                INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-Wduplicated-cond
+                          -Wnull-dereference>
             )
         endif()
         if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7.0)
@@ -98,25 +99,28 @@ if(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 6.0)
             target_compile_options(
-                compile_flags_target
-                INTERFACE -Wdocumentation -Wno-documentation-deprecated-sync
+                compile_flags_target INTERFACE -Wdocumentation
+                                               -Wno-documentation-deprecated-sync
             )
         endif()
     endif()
 endif(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
 
-if (DEFINED ${PROJECT_NAME}_EXTRA_FLAGS)
-message(STATUS "extra flags = ${${PROJECT_NAME}_EXTRA_FLAGS}")
- target_compile_options(
-                compile_flags_target
-                INTERFACE "${${PROJECT_NAME}_EXTRA_FLAGS}")
+if(DEFINED ${PROJECT_NAME}_EXTRA_FLAGS)
+    message(STATUS "extra flags = ${${PROJECT_NAME}_EXTRA_FLAGS}")
+    target_compile_options(
+        compile_flags_target INTERFACE "${${PROJECT_NAME}_EXTRA_FLAGS}"
+    )
 endif()
 # -------------------------------------------------------------
 # Extra definitions for visual studio
 # -------------------------------------------------------------
 if(MSVC)
 
-    target_compile_options(compile_flags_target INTERFACE -D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS)
+    target_compile_options(
+        compile_flags_target INTERFACE -D_CRT_SECURE_NO_WARNINGS
+                                       -D_SCL_SECURE_NO_WARNINGS
+    )
     # these next two should be global
     add_compile_options(/MP /EHsc)
     if(${PROJECT_NAME}_ENABLE_EXTRA_COMPILER_WARNINGS)
