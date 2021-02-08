6beefa5283cd3940dc5d159b4845829dd5c693f8
Philip Top <phlptp@gmail.com>
2021-02-08 15:05:01 -0800

    revert gtest version for gcc 4.8 and lower

--- config/AddGoogletest.cmake
+++ config/AddGoogletest.cmake
@@ -22,13 +22,19 @@ include(extraMacros)
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 3.8)
         set(gtest_version f3ef7e1)
-
+    else()
+        set(gtest_version 703bd9c)
+    endif()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 4.9)
+        set(gtest_version f3ef7e1)
     else()
         set(gtest_version 703bd9c)
     endif()
 else()
-    set(gtest_version f3ef7e1)
+     set(gtest_version f3ef7e1)
 endif()
+
 # depending on what the version is set to the git_clone command may need to change to
 # GIT_TAG||GIT_BRANCH|GIT_COMMIT
 
