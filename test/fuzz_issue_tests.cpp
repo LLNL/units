9974e6a7ba2922ed1de6ab8bc35289cb1748e4bc
Philip Top <phlptp@gmail.com>
2021-09-23 06:36:22 -0700

    reenable some tests

--- test/fuzz_issue_tests.cpp
+++ test/fuzz_issue_tests.cpp
@@ -370,4 +370,4 @@ TEST_P(measProblems, measFiles)
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(measFiles, measProblems, ::testing::Range(29, 30));
+INSTANTIATE_TEST_SUITE_P(measFiles, measProblems, ::testing::Range(0, 30));
