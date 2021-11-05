1553bff2e8a57a4ef72376793c8c7233c6ace250
Philip Top <phlptp@gmail.com>
2021-11-05 15:15:39 -0700

    update azure pipelines

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,6 @@ pygments_style = "default"
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-section_self_link = False
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
