5e16d6b00e1884ee91760489ba940d696ab01c9d
Philip Top <top1@llnl.gov>
2023-01-02 09:21:51 -0800

    Update copyright date to 2023 (#266)

--- config/compiler_flags.cmake
+++ config/compiler_flags.cmake
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Copyright (c) 2017-2022, Battelle Memorial Institute; Lawrence Livermore
+# Copyright (c) 2017-2023, Battelle Memorial Institute; Lawrence Livermore
 # National Security, LLC; Alliance for Sustainable Energy, LLC.
 # See the top-level NOTICE for additional details.
 # All rights reserved.
