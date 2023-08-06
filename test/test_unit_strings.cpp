e8731665539380bd420e4a8a17b0a3c1c5a48d68
Philip Top <top1@llnl.gov>
2023-08-06 06:45:07 -0700

    Reorder the string generation (#303)
    * Reorder the string generation to prioritize base units when generating strings
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- test/test_unit_strings.cpp
+++ test/test_unit_strings.cpp
@@ -60,7 +60,7 @@ TEST(unitStrings, squared)
 
 TEST(unitStrings, Combos)
 {
-    EXPECT_EQ(to_string(V / km), "V/km");
+    EXPECT_EQ(to_string(V / km), "mV/m");
     EXPECT_EQ(to_string(currency / MWh), "$/MWh");
     EXPECT_EQ(to_string(currency / mile), "$/mi");
     EXPECT_EQ(to_string(kW / gal), "kW/gal");
@@ -414,6 +414,14 @@ TEST(unitStrings, Hertz)
     EXPECT_EQ(to_string(precise::mega * precise::Hz), "MHz");
     EXPECT_EQ(to_string(precise::giga * precise::Hz), "GHz");
     EXPECT_EQ(to_string(precise::tera * precise::Hz), "THz");
+}
+
+TEST(unitStrings, singleBase)
+{
+    EXPECT_EQ(to_string(precise::kilo * precise::J / precise::mol), "kJ/mol");
+    EXPECT_EQ(
+        to_string(precise::kilo * precise::J / precise::rad.pow(2)),
+        "kJ/rad^2");
 }
 
 TEST(unitStrings, watthours)
@@ -1143,7 +1151,9 @@ TEST(userDefinedUnits, definitions)
 
     EXPECT_EQ(to_string(clucks * V), "clucks*V");
 
-    EXPECT_EQ(to_string(clucks * mol), "mol*clucks");
+    EXPECT_EQ(to_string(clucks * mol), "clucks*mol");
+
+    EXPECT_EQ(to_string(clucks * hp), "hp*clucks");
 
     EXPECT_EQ(to_string(clucks.pow(2) * kg), "kg*clucks^2");
 
