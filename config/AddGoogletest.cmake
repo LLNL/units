c62b823797bd63c8f4abdcd9ad73a01621ef1b38
Philip Top <phlptp@gmail.com>
2024-10-01 07:08:16 -0700

    only support google test for cmake 3.13 and higher

--- config/AddGoogletest.cmake
+++ config/AddGoogletest.cmake
@@ -47,11 +47,7 @@ if(NOT MSVC)
 endif()
 
 if(GOOGLE_TEST_INDIVIDUAL)
-    if(NOT CMAKE_VERSION VERSION_LESS 3.9)
-        include(GoogleTest)
-    else()
-        set(GOOGLE_TEST_INDIVIDUAL OFF)
-    endif()
+    include(GoogleTest)
 endif()
 
 function(add_unit_test test_source_file)
@@ -67,21 +63,11 @@ macro(add_gtest TESTNAME)
     target_link_libraries(${TESTNAME} PUBLIC gtest gmock gtest_main)
 
     if(GOOGLE_TEST_INDIVIDUAL)
-        if(CMAKE_VERSION VERSION_LESS 3.10)
-            gtest_add_tests(
-                TARGET ${TESTNAME}
-                TEST_PREFIX "${TESTNAME}."
-                TEST_LIST TmpTestList
-            )
-            set_tests_properties(${TmpTestList} PROPERTIES FOLDER "Tests")
-        else()
             gtest_discover_tests(
                 ${TESTNAME}
                 TEST_PREFIX "${TESTNAME}."
                 PROPERTIES FOLDER "Tests"
             )
-
-        endif()
     else()
         add_test(${TESTNAME} ${TESTNAME})
         set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
